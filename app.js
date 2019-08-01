ef0ce6a468652d59fefafb320cc74d8e1350022e
utkarsh maharana <utkarshm27@hotmail.com>
2019-08-01 17:58:56 +0530

    commenting out db lines

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 const express = require("express");
 const mysql = require("mysql");
 
-var db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "123456",
-  database: "nodemysql"
-});
+// var db = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "123456",
+//   database: "nodemysql"
+// });
 
 const app = express();
 
-db.connect(err => {
-  if (err) {
-    throw err;
-  }
+// db.connect(err => {
+//   if (err) {
+//     throw err;
+//   }
 
-  console.log("MySQL DB connected!");
-});
+//   console.log("MySQL DB connected!");
+// });
 
 app.listen("5000", () => {
   console.log("Server started on port 5000");
