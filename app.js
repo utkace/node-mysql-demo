b7d8cb591afc0d65930a5801b035b045a7f5b58b
utkarsh maharana <utkarshm27@hotmail.com>
2019-08-01 19:10:59 +0530

    Connected to sqlite server

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
 const express = require("express");
-const mysql = require("mysql");
+const sqlite3 = require("sqlite3").verbose();
 
-// var db = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "123456",
-//   database: "nodemysql"
-// });
+let db = new sqlite3.Database("./db/githubUsers.db", err => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log("Connected to the SQlite database.");
+});
 
 const app = express();
 
-// db.connect(err => {
-//   if (err) {
-//     throw err;
-//   }
-
-//   console.log("MySQL DB connected!");
-// });
+db.close(err => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log("Close the database connection.");
+});
 
 app.listen("5000", () => {
   console.log("Server started on port 5000");
